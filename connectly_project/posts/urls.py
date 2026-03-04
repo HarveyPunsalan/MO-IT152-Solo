157d7acaf25eb4c238f01eb8d511a5726fba04a2
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-03-04 13:07:22 +0800

    minor urls.py fixed

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
     path('posts/create/', CreatePostView.as_view(), name='post-create'),
     # My 3 new path
-    path('posts/<int:post_id>/like', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/comment', CommentPostView.as_view(), name='comment-post'),
-    path('posts/<int:post_id>/comments', GetPostCommentsView.as_view(), name='get-post-comments'),
+    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:post_id>/comment/', CommentPostView.as_view(), name='comment-post'),
+    path('posts/<int:post_id>/comments/', GetPostCommentsView.as_view(), name='get-post-comments'),
     # I push this under because django reads url top to bottom
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
